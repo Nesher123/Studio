f34005128a09b5223798491f2316d67273a23d12
Ofir <nesher123@gmail.com>
2017-12-24 15:02:48 +0200

    added exercise 8

---
+++ Exercise_08/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+let app = express();
+app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.json());
+
+app.get('/getFile/:filename',(req, res) => {
+	let fileToRead = req.params.filename || "showMyName.txt";
+	const src = fs.createReadStream(fileToRead);
+	src.on('error', handler);
+	src.pipe(res);
+});
+
+app.listen(app.get('port'), () => {
+	console.log(`Listening on port ${app.get('port')}!`);
+});
+
+// error-handling function (written as arrow function)
+const handler = err => console.log(err);
